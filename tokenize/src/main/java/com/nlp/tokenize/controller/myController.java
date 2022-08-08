4901c28848b4f9fad29183e40e9a07883e886ad9
Vaibhav Verma <56817583+vaibhav1199@users.noreply.github.com>
2022-08-08 08:48:27 +0530

    Add files via upload

---
+++ tokenize/src/main/java/com/nlp/tokenize/controller/myController.java
@@ -0,0 +1,63 @@
+package com.nlp.tokenize.controller;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.nlp.tokenize.model.Type;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.pipeline.CoreDocument;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+
+@RestController
+@RequestMapping(value="/nlp")
+public class myController {
+	String task;
+	String userinfo="";
+	final Type type = Type.valueOf("PERSON");
+	@Autowired
+	private StanfordCoreNLP stanfordcorenlp;
+
+	private restService rest= new restService();
+
+
+	@GetMapping(value="/info")
+	public void information(@RequestBody final String input){
+		CoreDocument coreDocument = new CoreDocument(input);
+		stanfordcorenlp.annotate(coreDocument);
+		List<CoreLabel> coreLabels = coreDocument.tokens();
+		// show my appointment or show vaibhav's appointment or show my data
+
+		for(CoreLabel coreLabel : coreLabels) {
+			//printing the all the tokens
+			String pos = coreLabel.getString(CoreAnnotations.PartOfSpeechAnnotation.class);
+
+			switch(pos) {
+				case "appointment":
+					task = "appointment";
+					break;
+				case "data":
+					task = "users";
+					break;
+				case "my":
+					userinfo = "vaibhav";
+					break;
+
+			}
+		}
+		List<String> info = name(coreLabels, type);
+		if(userinfo!="vaibhav") {
+			userinfo = info.get(0);
+		}
+		String url=task+userinfo;
+		rest.getService(url);
+	}
+	private List<String> name(List<CoreLabel> coreLabels, final Type type){
+		return coreLabels.stream().filter(coreLabel -> type.getName().equalsIgnoreCase(coreLabel.get(CoreAnnotations.NamedEntityTagAnnotation.class)))
+				.map(CoreLabel::originalText).collect(Collectors.toList());
+	}
+}
